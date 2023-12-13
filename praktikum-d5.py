c66ddb0e61e4eceafca986c176f4b7b9584a77d5
ajengniwahintan <153707486+ajengniwahintan@users.noreply.github.com>
2023-12-13 14:22:05 +0800

    upload file 1

---
+++ praktikum-d5.py
@@ -0,0 +1,78 @@
+Python 3.11.6 (tags/v3.11.6:8b6ee5b, Oct  2 2023, 14:57:12) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/user/Documents/pak-zaitun/program-d-231031108-ajeng/praktikum-d5.py
+Selamat Bergabung
+Selamat Bergabung
+Selamat Bergabung
+
+= RESTART: C:/Users/user/Documents/pak-zaitun/program-d-231031108-ajeng/praktikum-d5.py
+Selamat Bergabung
+Selamat Bergabung
+Selamat Bergabung
+Selamat Bergabung
+Selamat Bergabung
+Selamat Bergabung
+Selamat Bergabung
+Selamat Bergabung
+
+= RESTART: C:/Users/user/Documents/pak-zaitun/program-d-231031108-ajeng/praktikum-d5.py
+Selamat Bergabung
+Selamat Bergabung
+Selamat Bergabung
+
+= RESTART: C:/Users/user/Documents/pak-zaitun/program-d-231031108-ajeng/praktikum-d5.py
+Selamat Bergabung 1
+Selamat Bergabung 2
+Selamat Bergabung 3
+
+= RESTART: C:/Users/user/Documents/pak-zaitun/program-d-231031108-ajeng/praktikum-d5.py
+Selamat Bergabung 3
+Selamat Bergabung 2
+Selamat Bergabung 1
+
+= RESTART: C:/Users/user/Documents/pak-zaitun/program-d-231031108-ajeng/praktikum-d5.py
+Selamat Bergabung 10
+Selamat Bergabung 9
+Selamat Bergabung 8
+Selamat Bergabung 7
+Selamat Bergabung 6
+Selamat Bergabung 5
+Selamat Bergabung 4
+Selamat Bergabung 3
+Selamat Bergabung 2
+Selamat Bergabung 1
+>>> 
+= RESTART: C:/Users/user/Documents/pak-zaitun/program-d-231031108-ajeng/praktikum-d5.py
+Selamat Bergabung 10
+Selamat Bergabung 9
+Selamat Bergabung 8
+Selamat Bergabung 7
+Selamat Bergabung 6
+Selamat Bergabung 5
+Selamat Bergabung 4
+Selamat Bergabung 3
+Selamat Bergabung 2
+Selamat Bergabung 1
+Program berhenri disini !!!
+>>> 
+= RESTART: C:/Users/user/Documents/pak-zaitun/program-d-231031108-ajeng/praktikum-d5.py
+Selamat Bergabung 10
+Selamat Bergabung 9
+Selamat Bergabung 8
+Selamat Bergabung 7
+Selamat Bergabung 6
+Selamat Bergabung 5
+Selamat Bergabung 4
+Selamat Bergabung 3
+Selamat Bergabung 2
+Selamat Bergabung 1
+Program berhenti disini !!!
+>>> 
+= RESTART: C:/Users/user/Documents/pak-zaitun/program-d-231031108-ajeng/praktikum-d6.py
+Apakah Ingin Lanjutkan? (y/n)y
+Terima Kasih
+Apakah Ingin Lanjutkan? (y/n)y
+Terima Kasih
+Apakah Ingin Lanjutkan? (y/n)n
+Sampai Jumpa :D
